a0907c348e5b71d5c413e4cdc297c0c538b996e4
Ruby Iris Juric <ruby@srxl.me>
2024-05-03 17:38:34 +1000

    Expose exports from javascript flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
   inputs = {
     core.url = "github:auxolotl/packages";
+
+    javascript = {
+      url = "github:auxolotl/javascript";
+      inputs.top-level.follows = "";
+    };
   };
 
-  outputs = { self, core }: {
+  outputs = { self, core, javascript }: {
     inherit core;
+
+    javascript = javascript.topLevelOut { inherit core; };
   };
 }
